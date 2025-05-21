0dab20e84574e0b52a5e8573dafa2065dd6a33cc
7Digit-Main <it-support@7digit.io>
2025-05-21 11:36:30 +0300

    Add: logic to check if the user has already completed the onboarding

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,19 +1,42 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import * as SecureStore from "expo-secure-store";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { WelcomeScreen, MainScreen } from "#screens";
+import {
+  WelcomeScreen,
+  OnboardingScreen,
+  ProjectInformationScreen,
+  MainScreen,
+} from "#screens";
 import { RootStackParamList } from "#types";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+
 export const Navigator = () => {
+  const [isOnboarding, setIsOnboarding] = useState(false);
+  useEffect(() => {
+    const checkOnboarding = async () => {
+      const onboarding = await SecureStore.getItemAsync("onboarding");
+      setIsOnboarding(onboarding === "true");
+    };
+    checkOnboarding();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Welcome"
+        initialRouteName={isOnboarding ? "Welcome" : "Main"}
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
         <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen
+          name="ProjectInformation"
+          component={ProjectInformationScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
